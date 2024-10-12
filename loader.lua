9b0c72762a8f089cda95c6a22aaa5bf1b1886629
Harvox <134923684+Harvox@users.noreply.github.com>
2024-10-12 06:15:03 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,15 @@
+getgenv().version = "Beta 2.0"
+
+local games = {
+  papers = 13209672039
+}
+
+if game.PlaceId == games.papers then 
+  local success, err = pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Harvox/glacier-ware-2.0/refs/heads/main/papers%20pls.lua"))()
+  end)
+  
+  if not success then
+    warn("Failed to load script: " .. err)
+  end
+end
