c77db16c6c4a8a358cbbc4f92ee4b158624245af
Harvox <134923684+Harvox@users.noreply.github.com>
2024-11-14 14:32:49 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 if game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name == "Futuretops [VIP SERVER]" then 
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Harvox/glacier-ware-2.0/refs/heads/main/Futuretops.lua"))()
 end  
