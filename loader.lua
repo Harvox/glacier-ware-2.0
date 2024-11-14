fa42db6621774866fb4f408cf6d57e1aa75ca71b
Harvox <134923684+Harvox@users.noreply.github.com>
2024-11-14 14:32:25 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,14 +1,3 @@
+if game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name == "Futuretops [VIP SERVER]" then 
 
-local games = {
-  papers = 13209672039
-}
-
-if game.PlaceId == games.papers then 
-  local success, err = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Harvox/glacier-ware-2.0/refs/heads/main/papers%20pls.lua"))()
-  end)
-  
-  if not success then
-    warn("Failed to load script: " .. err)
-  end
-end
+end  
