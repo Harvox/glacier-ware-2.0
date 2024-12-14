2af41b0c24733ea822295c7e5071daf1880b7255
Harvox <134923684+Harvox@users.noreply.github.com>
2024-12-14 08:34:17 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 if game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name == "Futuretops [VIP SERVER]" then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Harvox/glacier-ware-2.0/refs/heads/main/Futuretops.lua"))()
 elseif game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name == "[beta] papers pls" then 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Harvox/glacier-ware-2.0/refs/heads/main/papers%20pls.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Harvox/glacier-ware-2.0/refs/heads/main/papers%20pls1.lua'))()
 elseif game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name == "🧬 Virus Border Roleplay" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Harvox/glacier-ware-2.0/refs/heads/main/Virusborder.lua"))()
 end  
