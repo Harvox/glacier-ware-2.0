e4c7ff3dd5b6da450cd178c8809cd71af950e3ef
Harvox <134923684+Harvox@users.noreply.github.com>
2024-12-14 10:40:18 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,11 @@
-
+shared.detected = "None"
 if game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name == "[beta] papers pls" then 
+shared.detected = "Papers Pls"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Harvox/glacier-ware-2.0/refs/heads/main/papers%20pls1.lua'))()
 elseif game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name == "🧬 Virus Border Roleplay" then
+shared.detected = "Virus Border Roleplay
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Harvox/glacier-ware-2.0/refs/heads/main/Virusborder.lua"))()
 else
+shared.detected = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Harvox/glacier-ware-2.0/refs/heads/main/Universal"))()
 end  
